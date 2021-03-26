0f64a9eb637c9e6226ccf48b250a55854f34ba5c
Nick Cao <nickcao@nichi.co>
2021-03-26 14:23:48 +0800

    extracted string_from_ptr to its own fn

--- src/lib.rs
+++ src/lib.rs
@@ -27,20 +27,25 @@ pub extern "C" fn pam_sm_authenticate(
     PamReturnCode::SUCCESS
 }
 
+fn string_from_ptr(ptr: *const c_char) -> Result<String, std::ffi::IntoStringError> {
+    let s = unsafe {CStr::from_ptr(ptr)};
+    s.to_owned().into_string()
+}
+
 pub fn extract_args(
     argc: c_int,
     argv: *const *const c_char,
-    ) -> Result<Vec<String>, std::str::Utf8Error> {
+    ) -> Result<Vec<String>, std::ffi::IntoStringError> {
     (0..argc)
-        .map(|i| unsafe { CStr::from_ptr(*argv.offset(i as isize)).to_str().map(|s| s.to_owned()) })
+        .map(|i| string_from_ptr(unsafe {*argv.offset(i as isize) }) )
         .collect()
 }
 
 pub fn pam_get_user(pamh: &PamHandle, prompt: &str) -> Result<String, Box<dyn Error>> {
-    let mut user: *const c_char = unsafe { std::mem::zeroed() };
+    let mut user: *const c_char = std::ptr::null();
     let prompt = CString::new(prompt)?;
     match pam_sys::wrapped::get_user(pamh, &mut user, prompt.as_ptr()) {
-        PamReturnCode::SUCCESS => Ok(unsafe { CStr::from_ptr(user).to_owned().into_string()? }),
+        PamReturnCode::SUCCESS => Ok(string_from_ptr(user)?),
         e => Err(Box::new(PamError::new(e))),
     }
 }
@@ -49,7 +54,7 @@ pub fn pam_get_item<T>(
     pamh: &PamHandle,
     item_type: PamItemType,
     ) -> Result<Option<&T>, Box<dyn Error>> {
-    let mut ptr: *const c_void = unsafe { std::mem::zeroed() };
+    let mut ptr: *const c_void = std::ptr::null();
     match pam_sys::wrapped::get_item(pamh, item_type, &mut ptr) {
         PamReturnCode::SUCCESS => Ok(unsafe { ptr.cast::<T>().as_ref() }),
         e => Err(Box::new(PamError::new(e))),
@@ -59,9 +64,7 @@ pub fn pam_get_item<T>(
 pub fn pam_get_string(pamh: &PamHandle, itype: PamItemType) -> Result<String, Box<dyn Error>> {
     let s = pam_get_item(pamh, itype)?;
     let s = s.ok_or(PamError::new(PamReturnCode::SYSTEM_ERR))?;
-    let s = unsafe {CStr::from_ptr(s)};
-    let s = s.to_owned().into_string()?;
-    Ok(s)
+    Ok(string_from_ptr(s)?)
 }
 
 pub fn pam_prompt(
@@ -76,10 +79,10 @@ pub fn pam_prompt(
         msg: prompt.as_ptr(),
         msg_style: style as i32,
     };
-    let mut resp: *mut PamResponse = unsafe { std::mem::zeroed() };
+    let mut resp: *mut PamResponse = std::ptr::null_mut();
     match PamReturnCode::from(callback(1, &mut msg, &mut resp, conv.data_ptr)) {
         PamReturnCode::SUCCESS => (),
         e => return Err(Box::new(PamError::new(e))),
     }
-    unsafe { resp.as_ref().ok_or("resp err")?.resp.as_ref()}.map_or(Ok("".to_string()), |r| Ok(unsafe { CStr::from_ptr(r).to_owned().into_string()?}))
+    unsafe { resp.as_ref().ok_or("resp err")?.resp.as_ref()}.map_or(Ok("".to_string()), |r| Ok(string_from_ptr(r)?))
 }
